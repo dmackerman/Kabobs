ca7c89e6c5a4b3f0919fc73478b63e6a66753971
Kelby Gassman <kelby@moduscreate.com>
2014-11-20 14:47:07 -0500

    dev

--- js/app.js
+++ js/app.js
@@ -1,11 +1,25 @@
 
 (function() {
 
-    var app = angular.module('kabobs', []);
+    var app = angular.module('kabobs', ['ngMaterial']);
 
-    app.controller('KabobController', function(){
+    app.controller('KabobController', ['$scope', function($scope) {
+        $scope.data = {};
+        $scope.data.geoff = false;
+        $scope.data.richard = false;
+
+        $scope.update = function(kabobs) {
+            $scope.data = angular.copy(kabobs);
+        };
+
+        $scope.reset = function () {
+            $scope.kabobs = angular.copy($scope.data);
+        };
+
+        $scope.reset();
 
     });
+
 
 
 })();
