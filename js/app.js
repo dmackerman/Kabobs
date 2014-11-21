866af13e86b5f2f1820d85a6bb52dbf6119d5c6f
Dave Ackerman <dave@moduscreate.com>
2014-11-21 11:19:27 -0500

    wrap entire application in a IIFE. avoids global scope polution.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,43 @@
-angular.module('kabobOrder', [])
-    .controller('KabobController', ['$scope', function($scope) {
-        $scope.master = {};
+(function() {
+    angular.module('kabobOrder', [])
+        .controller('KabobController', ['$scope', function($scope) {
+            $scope.master = {};
 
-        $scope.update = function(kabobs) {
-            $scope.master = angular.copy(kabobs);
-        };
+            $scope.update = function(kabobs) {
+                $scope.master = angular.copy(kabobs);
+            };
 
-        $scope.reset = function() {
-            $scope.kabobs = angular.copy($scope.master);
-        };
+            $scope.reset = function() {
+                $scope.kabobs = angular.copy($scope.master);
+            };
 
-        $scope.reset();
+            $scope.reset();
 
-    }])
+        }])
 
-.controller('MeatController', function() {
+    .controller('MeatController', function() {
 
-    this.products = [{
-        name: 'Beef Kobeeda',
-        id: 'beefKobeeda',
-        value: 'beefKobeeda'
-    }, {
-        name: 'Chicken Kobeeda',
-        id: 'chickenKobeeda',
-        value: 'chickenKobeeda'
-    }, {
-        name: 'Beef',
-        id: 'beef',
-        value: 'beef'
-    }, {
-        name: 'Chicken',
-        id: 'chicken',
-        value: 'chicken'
-    }, {
-        name: 'Lamb',
-        id: 'lamb',
-        value: 'lamb'
-    }];
-});
+        this.products = [{
+            name: 'Beef Kobeeda',
+            id: 'beefKobeeda',
+            value: 'beefKobeeda'
+        }, {
+            name: 'Chicken Kobeeda',
+            id: 'chickenKobeeda',
+            value: 'chickenKobeeda'
+        }, {
+            name: 'Beef',
+            id: 'beef',
+            value: 'beef'
+        }, {
+            name: 'Chicken',
+            id: 'chicken',
+            value: 'chicken'
+        }, {
+            name: 'Lamb',
+            id: 'lamb',
+            value: 'lamb'
+        }];
+    });
+
+})();
