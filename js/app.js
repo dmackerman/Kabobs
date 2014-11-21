7d8d67f9a74210cd2e05ea39e3fb818a59dc6c9a
Kelby Gassman <kelby@moduscreate.com>
2014-11-21 10:46:23 -0500

    Added meat var and repeat product checkboxes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,50 @@
 
-angular.module('kabobs', [])
-    .controller('KabobController', ['$scope', function($scope) {
+angular.module('kabobOrder', [])
+    .controller('KabobController', ['$scope', function ($scope) {
         $scope.master = {};
 
-        $scope.update = function(kabobs) {
+        $scope.update = function (kabobs) {
             $scope.master = angular.copy(kabobs);
         };
 
-        $scope.reset = function() {
-            $scope.user = angular.copy($scope.master);
+        $scope.reset = function () {
+            $scope.kabobs = angular.copy($scope.master);
         };
 
         $scope.reset();
-    }]);
+
+    }])
+
+    .controller('MeatController', function () {
+
+        this.products = meats;
+    });
+
+    var meats = [
+        {
+            name: 'Beef Kobeeda',
+            id: 'beefKobeeda',
+            value: 'beefKobeeda'
+        },
+        {
+            name: 'Chicken Kobeeda',
+            id: 'chickenKobeeda',
+            value: 'chickenKobeeda'
+        },
+        {
+            name: 'Beef',
+            id: 'beef',
+            value: 'beef'
+        },
+        {
+            name: 'Chicken',
+            id: 'chicken',
+            value: 'chicken'
+        },
+        {
+            name: 'Lamb',
+            id: 'lamb',
+            value: 'lamb'
+        }
+
+    ];
